93c1b437956ff5172425ee027a6c45c2c177fc20
ITlearning <yo7504@naver.com>
2021-10-04 22:30:12 +0900

    Xcode init

---
+++ KeepIt-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeepIt-iOS
+//
+//  Created by ITlearning on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    
+    
+}
+
