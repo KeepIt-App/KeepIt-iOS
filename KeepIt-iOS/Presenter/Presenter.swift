d0a2fafb3920ccad887b2ff7231c4eb399461bf6
ITlearning <yo7504@naver.com>
2021-10-08 20:24:53 +0900

    [Add] MVC -> MVP 디자인 패턴으로 변경 후 기초 세팅

---
+++ KeepIt-iOS/Presenter/Presenter.swift
@@ -0,0 +1,16 @@
+//
+//  Presenter.swift
+//  KeepIt-iOS
+//
+//  Created by ITlearning on 2021/10/08.
+//
+
+import UIKit
+
+protocol MainViewProtocol {
+    
+}
+
+class Presenter: MainViewProtocol {
+    
+}
